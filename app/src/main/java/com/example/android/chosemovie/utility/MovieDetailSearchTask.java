0591b1f86c3218b3a7c952d66cab4c1d983b7587
zuoqi <774765813@qq.com>
2018-01-29 23:23:04 +0800

    Fix the second task.

--- app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
+++ app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
@@ -1,10 +1,13 @@
 package com.example.android.chosemovie.utility;
 
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.android.chosemovie.PicRecAdapter;
 import com.example.android.chosemovie.base.MovieReviews;
+import com.example.android.chosemovie.base.MovieSingleInfo;
+import com.example.android.chosemovie.base.MovieTrailers;
 
 import org.json.JSONException;
 
@@ -15,54 +18,71 @@ import java.net.URL;
  * Created by milkdz on 2018/1/28.
  */
 
-public class MovieDetailSearchTask extends AsyncTask<Integer, Void, Object[]> {
+public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieSingleInfo> {
 
     private String TAG = "MovieDetailSearchTask";
     private boolean DBG = true;
     private OpenMovieInfoJson openMovieInfoJson;
     private final int ID_VIDEO = PicRecAdapter.ID_VIDEO;
     private final int ID_REVIEWS = PicRecAdapter.ID_REVIEWS;
-    private String SID;
 
 
-    public MovieDetailSearchTask(String id, OpenMovieInfoJson openMovieInfoJson) {
-        this.SID = id;
+    public MovieDetailSearchTask(OpenMovieInfoJson openMovieInfoJson) {
         this.openMovieInfoJson = openMovieInfoJson;
     }
 
     @Override
-    protected Object[] doInBackground(Integer... integers) {
+    protected MovieSingleInfo doInBackground(String... sid) {
 
-        if (integers.length == 0) {
+        if (sid.length == 0) {
             return null;
         }
+        String sId = sid[0];
+        MovieReviews[] movieReviews = getMoviesReviews(sId);
+        MovieTrailers movieTrailers = getMoviesTrailers(sId);
+        return new MovieSingleInfo(movieReviews, movieTrailers);
+    }
 
-        int choseMood = integers[0];
-        String jsonData;
-        switch (choseMood) {
-            case ID_VIDEO: {
-                URL url = NetWorkUtils.buildUrlForDifSort(choseMood, SID);
-                try {
-                    jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
-                    if (DBG)  Log.d(TAG,"------json_video----- "+jsonData);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            break;
-            case ID_REVIEWS: {
-                URL url = NetWorkUtils.buildUrlForDifSort(choseMood, SID);
-                try {
-                    jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
-                    MovieReviews[] movieReviewsArr = openMovieInfoJson.getMovieReviews(jsonData);
-                    return movieReviewsArr;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            break;
+    /**
+     * get movie reviews array
+     *
+     * @param sId movie id
+     * @return array of MovieReviews
+     */
+    @Nullable
+    private MovieReviews[] getMoviesReviews(String sId) {
+
+        URL url = NetWorkUtils.buildUrlForDifSort(ID_REVIEWS, sId);
+        MovieReviews[] movieReviews;
+        try {
+            String jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
+            movieReviews = openMovieInfoJson.getMovieReviews(jsonData);
+            return movieReviews;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get object of MoviesTrailers
+     *
+     * @param sId movie id
+     * @return object
+     */
+    @Nullable
+    private MovieTrailers getMoviesTrailers(String sId) {
+        URL url = NetWorkUtils.buildUrlForDifSort(ID_VIDEO, sId);
+        try {
+            String jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
+            MovieTrailers movieTrailers = openMovieInfoJson.getMovieTrailers(jsonData);
+            return movieTrailers;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
