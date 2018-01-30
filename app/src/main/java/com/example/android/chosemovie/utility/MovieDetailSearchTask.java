32c11ceed2904380d1cd3c62b056b97fdf358a00
zuoqi <774765813@qq.com>
2018-01-30 23:21:16 +0800

    Update

--- app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
+++ app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
@@ -1,10 +1,17 @@
 package com.example.android.chosemovie.utility;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.android.chosemovie.PicRecAdapter;
+import com.example.android.chosemovie.R;
 import com.example.android.chosemovie.base.MovieReviews;
 import com.example.android.chosemovie.base.MovieSingleInfo;
 import com.example.android.chosemovie.base.MovieTrailers;
@@ -13,26 +20,32 @@ import org.json.JSONException;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by milkdz on 2018/1/28.
  */
 
-public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieSingleInfo> {
+public class MovieDetailSearchTask extends AsyncTask<String, Void, Button[]> {
 
     private String TAG = "MovieDetailSearchTask";
     private boolean DBG = true;
     private OpenMovieInfoJson openMovieInfoJson;
     private final int ID_VIDEO = PicRecAdapter.ID_VIDEO;
     private final int ID_REVIEWS = PicRecAdapter.ID_REVIEWS;
+    private RelativeLayout relativeLayout;
+    private Context context;
 
 
-    public MovieDetailSearchTask(OpenMovieInfoJson openMovieInfoJson) {
+    public MovieDetailSearchTask(OpenMovieInfoJson openMovieInfoJson, RelativeLayout relativeLayout,
+                                 Context context) {
         this.openMovieInfoJson = openMovieInfoJson;
+        this.relativeLayout = relativeLayout;
+        this.context = context;
     }
 
     @Override
-    protected MovieSingleInfo doInBackground(String... sid) {
+    protected Button[] doInBackground(String... sid) {
 
         if (sid.length == 0) {
             return null;
@@ -40,7 +53,31 @@ public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieSingleIn
         String sId = sid[0];
         MovieReviews[] movieReviews = getMoviesReviews(sId);
         MovieTrailers movieTrailers = getMoviesTrailers(sId);
-        return new MovieSingleInfo(movieReviews, movieTrailers);
+        ArrayList<Uri> uriArrayList = movieTrailers.getTrailersUrl();
+        int num = uriArrayList.size();
+        Log.d(TAG,"====> num " + num);
+        Button[] buttons = new Button[num];
+        for (int i = 0; i < num; i++) {
+            Button button = new Button(context);
+            button.setLayoutParams(new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            String show = "Trailer " + (i + 1);
+            button.setText(show);
+            buttons[i] = button;
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Button[] movieSingleInfo) {
+        if (movieSingleInfo == null) {
+            return;
+        }
+        for (int i=0;i<movieSingleInfo.length;i++){
+            relativeLayout.addView(movieSingleInfo[i]);
+            Log.d(TAG,"===============> movieSingleInfo[i]");
+        }
     }
 
     /**
