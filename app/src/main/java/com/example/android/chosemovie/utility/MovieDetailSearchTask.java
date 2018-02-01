c6bbe5a200178fe9b36df2b6fc8264856ee95479
zuoqi <774765813@qq.com>
2018-02-01 23:01:39 +0800

    Update

--- app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
+++ app/src/main/java/com/example/android/chosemovie/utility/MovieDetailSearchTask.java
@@ -1,6 +1,7 @@
 package com.example.android.chosemovie.utility;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
@@ -27,7 +28,7 @@ import java.util.ArrayList;
  * Created by milkdz on 2018/1/28.
  */
 
-public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers> {
+public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers> implements View.OnClickListener{
 
     private String TAG = "MovieDetailSearchTask";
     private boolean DBG = true;
@@ -35,12 +36,11 @@ public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers
     private final int ID_VIDEO = PicRecAdapter.ID_VIDEO;
     private final int ID_REVIEWS = PicRecAdapter.ID_REVIEWS;
     private LinearLayout[] linearLayouts;
-    private LinearLayout linearLayoutChild;
     private Context context;
+    private ArrayList<Uri> uriArrayList;
 
-
-    public MovieDetailSearchTask(OpenMovieInfoJson openMovieInfoJson,
-                                 LinearLayout[] linearLayouts,Context context) {
+    public MovieDetailSearchTask(OpenMovieInfoJson openMovieInfoJson, LinearLayout[] linearLayouts,
+                                 Context context) {
         this.openMovieInfoJson = openMovieInfoJson;
         this.linearLayouts = linearLayouts;
         this.context = context;
@@ -55,12 +55,9 @@ public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers
         String sId = sid[0];
         MovieReviews[] movieReviews = getMoviesReviews(sId);
         MovieTrailers movieTrailers = getMoviesTrailers(sId);
-        ArrayList<Uri> uriArrayList = movieTrailers.getTrailersUrl();
+        uriArrayList = movieTrailers.getTrailersUrl();
         int num = uriArrayList.size();
-        Log.d(TAG, "====> num " + num);
-
-
-
+        if (DBG) Log.d(TAG, "====> num " + num);
         return movieTrailers;
     }
 
@@ -82,12 +79,14 @@ public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers
                     Log.d(TAG, show);
                     button.setText(show);
                     button.setTag(i);
+                    button.setOnClickListener(this);
                     i++;
                     linearLayouts[j].addView(button);
                 }else {
                     break;
                 }
             }
+            i --;
         }
     }
 
@@ -133,5 +132,28 @@ public class MovieDetailSearchTask extends AsyncTask<String, Void, MovieTrailers
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * Open the web page
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        int iTag = (int) v.getTag();
+        if(uriArrayList == null){
+            return;
+        }
+        Uri uri = uriArrayList.get(iTag);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        /*
+         * This is a check we perform with every implicit Intent that we launch. In some cases,
+         * the device where this code is running might not have an Activity to perform the action
+         * with the data we've specified. Without this check, in those cases your app would crash.
+         */
+        if(intent.resolveActivity(context.getPackageManager()) != null){
+            context.startActivity(intent);
+        }
     }
 }
