c55dcb2e284e80cc8434446dd21fd78208acc144
zuoqi <774765813@qq.com>
2018-02-04 21:44:37 +0800

    add SQL base

---
+++ app/src/main/java/com/example/android/chosemovie/data/SQLBaseInfo.java
@@ -0,0 +1,15 @@
+package com.example.android.chosemovie.data;
+
+/**
+ * Created by milkdz on 2018/2/4.
+ */
+
+public class SQLBaseInfo {
+    public static final String CREATE_TABLE = "CREATE TABLE ";
+
+    public static final String INT_PRIMARY_KEY = " INTEGER PRIMARY KEY AUTOINCREMENT, ";
+
+    public static final String TEXT_NO_NULL = " TEXT NOT NULL, ";
+
+    public static final String DROP_TABLE = " DROP TABLE IF EXISTS ";
+}
