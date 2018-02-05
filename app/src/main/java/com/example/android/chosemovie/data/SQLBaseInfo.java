c52aa6e87cd7067326a53bd12813b96b5a88fb0f
zuoqi <774765813@qq.com>
2018-02-05 23:29:40 +0800

    Update SQL db

--- app/src/main/java/com/example/android/chosemovie/data/SQLBaseInfo.java
+++ app/src/main/java/com/example/android/chosemovie/data/SQLBaseInfo.java
@@ -4,12 +4,13 @@ package com.example.android.chosemovie.data;
  * Created by milkdz on 2018/2/4.
  */
 
-public class SQLBaseInfo {
-    public static final String CREATE_TABLE = "CREATE TABLE ";
+public interface SQLBaseInfo {
 
-    public static final String INT_PRIMARY_KEY = " INTEGER PRIMARY KEY AUTOINCREMENT, ";
+    String CREATE_TABLE = "CREATE TABLE ";
 
-    public static final String TEXT_NO_NULL = " TEXT NOT NULL, ";
+    String INT_PRIMARY_KEY = " INTEGER PRIMARY KEY AUTOINCREMENT, ";
 
-    public static final String DROP_TABLE = " DROP TABLE IF EXISTS ";
+    String TEXT_NO_NULL = " TEXT NOT NULL, ";
+
+    String DROP_TABLE = " DROP TABLE IF EXISTS ";
 }
