530ed83fb0749fbd8eba24698d2a82cddc615019
zuoqi <774765813@qq.com>
2018-02-08 23:24:36 +0800

    Change task

--- app/src/main/java/com/example/android/chosemovie/sync/MovieSyncTask.java
+++ app/src/main/java/com/example/android/chosemovie/sync/MovieSyncTask.java
@@ -3,7 +3,9 @@ package com.example.android.chosemovie.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.util.Log;
 
+import com.example.android.chosemovie.data.BaseDataInfo;
 import com.example.android.chosemovie.db.MovieInfoContract;
 import com.example.android.chosemovie.utility.NetWorkUtils;
 import com.example.android.chosemovie.utility.OpenMovieInfoJson;
@@ -19,11 +21,21 @@ import java.net.URL;
 
 public class MovieSyncTask {
 
+    private static String TAG = "MovieSyncTask";
+    private static boolean DBG = true;
+
     synchronized public static void syncMovie(OpenMovieInfoJson openMovieInfoJson, Context context) {
+
+        syncMovie(openMovieInfoJson,context,BaseDataInfo.RATE_DATE_MODE);
+        syncMovie(openMovieInfoJson,context,BaseDataInfo.POPULAR_MODE);
+    }
+
+    synchronized private static void syncMovie(OpenMovieInfoJson openMovieInfoJson, Context context,int mode){
         try {
-            URL url = NetWorkUtils.buildUrlForDifSort(0);
+            if (DBG) Log.d(TAG,"run here");
+            URL url = NetWorkUtils.buildUrlForDifSort(mode);
             String sjson = NetWorkUtils.getResponseFromHttpUrl(url);
-            ContentValues[] contentValues = openMovieInfoJson.getDataFromMovieJson(sjson);
+            ContentValues[] contentValues = openMovieInfoJson.getDataFromMovieJson(sjson,mode);
             if (contentValues != null && contentValues.length != 0){
                 ContentResolver contentResolver = context.getContentResolver();
                 contentResolver.delete(MovieInfoContract.MovieInfos.CONTENT_URI,null,null);
@@ -33,8 +45,6 @@ public class MovieSyncTask {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-
         }
     }
 }
