59a4527388c1c0b2ff9630091c761cac356c205b
zuoqi <774765813@qq.com>
2018-02-10 22:33:54 +0800

    Update db base

--- app/src/main/java/com/example/android/chosemovie/db/MovieInfoContract.java
+++ app/src/main/java/com/example/android/chosemovie/db/MovieInfoContract.java
@@ -2,9 +2,13 @@ package com.example.android.chosemovie.db;
 
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
 
+import com.example.android.chosemovie.base.MovieInfo;
 import com.example.android.chosemovie.data.BaseDataInfo;
 import com.example.android.chosemovie.data.SQLBaseInfo;
+
+import static com.example.android.chosemovie.db.MovieInfoContract.MovieInfos.CONTENT_URI;
 
 /**
  * Created by milkdz on 2018/2/4.
@@ -13,10 +17,10 @@ import com.example.android.chosemovie.data.SQLBaseInfo;
 public class MovieInfoContract {
 
     public static final String CONTENT_AUTHORITY = "com.example.android.chosemovie";
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_MOVIE = "movie";
 
-    public static final class MovieInfos implements BaseColumns{
+    public static final class MovieInfos implements BaseColumns {
 
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE)
                 .build();
@@ -26,38 +30,39 @@ public class MovieInfoContract {
         public static final String COLUMN_MOVIE_NAME = "title";
         public static final String COLUMN_MOVIE_DATE = "pub_date";
         public static final String COLUMN_MOVIE_VOTE = "vote";
+        public static final String COLUMN_MOVIE_OVER_VIEW = "over_view";
         public static final String COLUMN_MOVIE_REVIEW = "review";
         public static final String COLUMN_MOVIE_TRAILER = "trailers";
         public static final String COLUMN_MOVIE_POSTER_IMAGE = "poster_image";
         public static final String COLUMN_MOVIE_BACK_IMAGE = "back_image";
-        public static final String COLUMN_MOVIE_FAVORITE = "favorite";
         public static final String COLUMN_MOVIE_SORT = "sort";
     }
 
-    public static final class MovieReviews implements BaseColumns{
-        public static final String TABLE_NAME = "movieReviews_";
-        public static final String COLUMN_MOVIE_REVIEWS_AUTHOR = "author";
-        public static final String COLUMN_MOVIE_REVIEW_CONTENT = "content";
-    }
-
-    public static final class MovieTrailers implements BaseColumns{
-        public static final String TABLE_NAME = "movieTrailers_";
-        public static final String COLUMN_MOVIE_TRAILER_URI = "trailers_uri";
-    }
-
     public static final String[] MAIN_MOVIE_UI = new String[]{
+            MovieInfos._ID,
             MovieInfos.COLUMN_MOVIE_POSTER_IMAGE,
-            MovieInfos.COLUMN_MOVIE_SORT
+            MovieInfos.COLUMN_MOVIE_SORT,
+    };
+
+    public static final String[] CHILD_MOVIE_UI = new String[]{
+            MovieInfos.COLUMN_MOVIE_ID,
+            MovieInfos.COLUMN_MOVIE_BACK_IMAGE,
+            MovieInfos.COLUMN_MOVIE_DATE,
+            MovieInfos.COLUMN_MOVIE_NAME,
+            MovieInfos.COLUMN_MOVIE_VOTE,
+            MovieInfos.COLUMN_MOVIE_OVER_VIEW,
+            MovieInfos.COLUMN_MOVIE_TRAILER,
+            MovieInfos.COLUMN_MOVIE_REVIEW
     };
 
     /**
-     * Build content Uri for movie detail
+     * Build content Uri for movie detail with movie id
      *
      * @param base
      * @return
      */
-    public static Uri buildContentForMovieDetail(String base){
-        return BASE_CONTENT_URI.buildUpon().appendPath(base).build();
+    public static Uri buildContentForMovieDetail(String base) {
+        return CONTENT_URI.buildUpon().appendPath(base).build();
     }
 
     /**
@@ -66,8 +71,25 @@ public class MovieInfoContract {
      * @param select select value
      * @return select string
      */
-    public static String getSelect(int select){
-        return MovieInfos.COLUMN_MOVIE_SORT + "=" + select + SQLBaseInfo.OR
-                + MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.BOTH_POPULAR_RATE_MODE;
+    @NonNull
+    public static String getSelect(int select) {
+        switch (select) {
+            case BaseDataInfo.POPULAR_MODE:{
+                return MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.FAVORITE_POPULAR_MODE
+                        + SQLBaseInfo.OR
+                        + MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.UN_FAVORITE_POPULAR;
+            }
+            case BaseDataInfo.RATE_DATE_MODE: {
+                return MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.FAVORITE_RATE_DATE_MODE
+                        + SQLBaseInfo.OR
+                        + MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.UN_FAVORITE_RATE_DATE_MODE;
+            }
+            case BaseDataInfo.FAVORITE_MODE: {
+                return MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.FAVORITE_RATE_DATE_MODE
+                        + SQLBaseInfo.OR
+                        + MovieInfos.COLUMN_MOVIE_SORT + "=" + BaseDataInfo.FAVORITE_POPULAR_MODE;
+            }
+        }
+        return null;
     }
 }
