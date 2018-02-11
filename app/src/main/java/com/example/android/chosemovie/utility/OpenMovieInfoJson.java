4ed5921a608186baf5e792912c35ddbffc52f9ae
zuoqi <774765813@qq.com>
2018-02-11 23:17:05 +0800

    update openmoviejson

--- app/src/main/java/com/example/android/chosemovie/utility/OpenMovieInfoJson.java
+++ app/src/main/java/com/example/android/chosemovie/utility/OpenMovieInfoJson.java
@@ -1,7 +1,10 @@
 package com.example.android.chosemovie.utility;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.android.chosemovie.base.MovieInfo;
@@ -14,8 +17,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -26,8 +31,6 @@ public class OpenMovieInfoJson {
 
     private String TAG = "OpenMovieInfoJson";
     private boolean DBG = true;
-
-    private String FAVORITE_TEXT_DEFAULT = "收藏此电影";
 
     private String QUERY = "results";
     private String POSTER_PATH = "poster_path";
@@ -42,11 +45,8 @@ public class OpenMovieInfoJson {
     private String REVIEWS_CONTENT = "content";
 
     private String VIDEO_TRAILERS_KEY = "key";
-
+    private SharedPreferences sharedPreferences;
     public static OpenMovieInfoJson openMovieInfoJson;
-
-    private MovieInfo movieInfo;
-    private MovieReviews movieReviews;
 
     private OpenMovieInfoJson() {
     }
@@ -59,104 +59,77 @@ public class OpenMovieInfoJson {
         return openMovieInfoJson;
     }
 
-    public ContentValues[] getDataFromMovieJson(String jsonString,int mode) throws JSONException {
+    public ContentValues[] getDataFromMovieJson(String jsonString, int mode, Context context) throws JSONException {
         if (DBG) Log.d(TAG, "jsonString read from themoviedb.org is : " + jsonString);
+        if (DBG) Log.d(TAG, "jsonString read from themoviedb.org == mode : " + mode);
+
+        sharedPreferences = context.getSharedPreferences(BaseDataInfo.MOVIE_PREFERENCE,Context.MODE_PRIVATE);
         JSONObject movieJson = new JSONObject(jsonString);
         JSONArray movieArray = movieJson.getJSONArray(QUERY);
         ContentValues[] contentValues = new ContentValues[movieArray.length()];
         JSONObject jsonObject;
         String sPath;
         String sPath_back;
-
-        String overView;
         for (int i = 0; i < movieArray.length(); i++) {
             jsonObject = movieArray.getJSONObject(i);
-
-            String sReview = MovieInfoContract.MovieReviews.TABLE_NAME + i;
-            String sTrailers = MovieInfoContract.MovieTrailers.TABLE_NAME + i;
+            String MovieID = jsonObject.getString(ID);
+            int favorite = sharedPreferences.getInt(MovieID,BaseDataInfo.UN_FAVORITE);
+            int favoriteInt = favorite + mode;
+            String sReview = getMoviesReviews(MovieID);
+            String sTrailers = getMoviesTrailers(MovieID);
             sPath = BaseDataInfo.baseUrl + jsonObject.getString(POSTER_PATH);
-           // sId = BaseDataInfo.originUrl + jsonObject.getString(ID);
             sPath_back = BaseDataInfo.baseUrl + jsonObject.getString(BACK_PATH);
-            overView =  jsonObject.getString(OVER_VIEW);
+
             ContentValues contentValueSingle = new ContentValues();
-            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_ID,jsonObject.getString(ID));
+            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_ID,MovieID);
             contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_DATE,jsonObject.getString(RELEASE_DATE));
             contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_NAME,jsonObject.getString(TITLE));
             contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_VOTE,jsonObject.getString(VOTE_AVERAGE));
             contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_POSTER_IMAGE,sPath);
             contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_BACK_IMAGE,sPath_back);
-            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_FAVORITE,0);
-            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_SORT,mode);
-            Log.d("========mode",mode+"");
-            //contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_REVIEW,sReview);
-            //contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_TRAILER,sTrailers);
+            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_SORT,favoriteInt);
+            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_OVER_VIEW,jsonObject.getString(OVER_VIEW));
+            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_REVIEW,sReview);
+            contentValueSingle.put(MovieInfoContract.MovieInfos.COLUMN_MOVIE_TRAILER,sTrailers);
             contentValues[i] = contentValueSingle;
+            Log.d("========mode ",favoriteInt+"");
+            Log.d("========sReview",sReview + "");
+            Log.d("========sTrailers",sTrailers + "");
         }
         return contentValues;
     }
 
     /**
-     * Analysis the jsonData from NetWorkUtils.getResponseFromHttpUrl
+     * get movie reviews array
      *
-     * @param jsonString json read from themoviedb.org
-     * @return the object of MovieInfo
-     * @throws JSONException
-     * @see NetWorkUtils
+     * @param sId movie id
+     * @return array of MovieReviews
      */
-    public MovieInfo[] getDataFromMovieJson1(String jsonString) throws JSONException {
+    @Nullable
+    private String getMoviesReviews(String sId) {
 
-        MovieInfo[] movieData;
-        if (DBG) Log.d(TAG, "jsonString read from themoviedb.org is : " + jsonString);
-        JSONObject movieJson = new JSONObject(jsonString);
-        JSONArray movieArray = movieJson.getJSONArray(QUERY);
-        JSONObject jsonObject;
-        movieData = new MovieInfo[movieArray.length()];
-        String sPath;
-        String sId;
-        String sPath_back;
-        String title;
-        String voteAver;
-        String pubDate;
-        String overView;
-        for (int i = 0; i < movieArray.length(); i++) {
-            jsonObject = movieArray.getJSONObject(i);
-            sPath = BaseDataInfo.baseUrl + jsonObject.getString(POSTER_PATH);
-            sId = BaseDataInfo.originUrl + jsonObject.getString(ID);
-            title = jsonObject.getString(TITLE);
-            sPath_back = BaseDataInfo.baseUrl + jsonObject.getString(BACK_PATH);
-            overView = jsonObject.getString(OVER_VIEW);
-            voteAver = jsonObject.getString(VOTE_AVERAGE);
-            pubDate = jsonObject.getString(RELEASE_DATE);
-            movieInfo = new MovieInfo(sId, sPath, title, sPath_back, overView, voteAver, pubDate,
-                    0,FAVORITE_TEXT_DEFAULT);
-            movieData[i] = movieInfo;
+        URL url = NetWorkUtils.buildUrlForDifSort(BaseDataInfo.ID_REVIEWS, sId);
+        Log.d(TAG,"getMoviesReviews = URL = " + url);
+        try {
+            String jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
+            Log.d(TAG,"getMoviesReviews = jsonData = " + jsonData);
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray(QUERY);
+            JSONObject jsonSingleObject;
+            String reviewsArr = "";
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonSingleObject = jsonArray.getJSONObject(i);
+                String author = jsonSingleObject.getString(REVIEWS_AUTHOR);
+                String content = jsonSingleObject.getString(REVIEWS_CONTENT);
+                reviewsArr = reviewsArr + author + "," + content + ";";
+            }
+            return reviewsArr;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        return movieData;
-    }
-
-    /**
-     * Get detail messages about Reviews
-     *
-     * @param jsonString json string of detail message
-     * @return object MovieInfo
-     * @throws JSONException
-     */
-    public MovieReviews[] getMovieReviews(String jsonString) throws JSONException {
-
-        if (DBG) Log.d(TAG, "++++++jsonData++++++++ === > " + jsonString);
-        MovieReviews[] movieReviewsArr;
-        JSONObject jsonObject = new JSONObject(jsonString);
-        JSONArray jsonArray = jsonObject.getJSONArray(QUERY);
-        movieReviewsArr = new MovieReviews[jsonArray.length()];
-        JSONObject jsonSingleObject;
-        for (int i = 0; i < jsonArray.length(); i++) {
-            jsonSingleObject = jsonArray.getJSONObject(i);
-            String author = jsonSingleObject.getString(REVIEWS_AUTHOR);
-            String content = jsonSingleObject.getString(REVIEWS_CONTENT);
-            movieReviews = new MovieReviews(author, content);
-            movieReviewsArr[i] = movieReviews;
-        }
-        return movieReviewsArr;
+        return null;
     }
 
     /**
@@ -166,20 +139,141 @@ public class OpenMovieInfoJson {
      * @return MovieTrailers object
      * @throws JSONException
      */
-    public MovieTrailers getMovieTrailers(String jsonString) throws JSONException {
+    private String getMovieTrailerss(String jsonString) throws JSONException {
 
         if (DBG) Log.d(TAG,"Movie Trailers is " + jsonString);
         JSONObject jsonObject = new JSONObject(jsonString);
         JSONArray jsonArray = jsonObject.getJSONArray(QUERY);
         int length = jsonArray.length();
-        ArrayList<Uri> arrayList = new ArrayList<>();
         JSONObject jsonSingleObject;
+        String trailes = "";
         for (int i = 0; i < length; i++) {
             jsonSingleObject = jsonArray.getJSONObject(i);
             String v = jsonSingleObject.getString(VIDEO_TRAILERS_KEY);
             Uri uri = NetWorkUtils.buildUrlForYoutube(v);
-            arrayList.add(i, uri);
+            trailes = trailes + ";" + uri;
         }
+        return trailes;
+    }
+
+
+
+    /**
+     * Get object of MoviesTrailers
+     *
+     * @param movieId movie id
+     * @return object
+     */
+    @Nullable
+    private String getMoviesTrailers(String movieId) {
+
+        URL url = NetWorkUtils.buildUrlForDifSort(BaseDataInfo.ID_MOVIE, movieId);
+        try {
+            String jsonData = NetWorkUtils.getResponseFromHttpUrl(url);
+            String trailersArr = getMovieTrailerss(jsonData);
+            return trailersArr;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+
+     * Get detail messages about Reviews
+
+     *
+
+     * @param jsonString json string of detail message
+
+     * @return object MovieInfo
+
+     * @throws JSONException
+
+     */
+
+    public MovieReviews[] getMovieReviews(String jsonString) throws JSONException {
+
+
+
+        if (DBG) Log.d(TAG, "++++++jsonData++++++++ === > " + jsonString);
+        MovieReviews movieReviews;
+        MovieReviews[] movieReviewsArr;
+
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        JSONArray jsonArray = jsonObject.getJSONArray(QUERY);
+
+        movieReviewsArr = new MovieReviews[jsonArray.length()];
+
+        JSONObject jsonSingleObject;
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            jsonSingleObject = jsonArray.getJSONObject(i);
+
+            String author = jsonSingleObject.getString(REVIEWS_AUTHOR);
+
+            String content = jsonSingleObject.getString(REVIEWS_CONTENT);
+
+            movieReviews = new MovieReviews(author, content);
+
+            movieReviewsArr[i] = movieReviews;
+
+        }
+
+        return movieReviewsArr;
+
+    }
+
+
+
+    /**
+
+     * Get Movie Trailers
+
+     *
+
+     * @param jsonString json string of array message
+
+     * @return MovieTrailers object
+
+     * @throws JSONException
+
+     */
+
+    public MovieTrailers getMovieTrailers(String jsonString) throws JSONException {
+
+
+
+        if (DBG) Log.d(TAG,"Movie Trailers is " + jsonString);
+
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        JSONArray jsonArray = jsonObject.getJSONArray(QUERY);
+
+        int length = jsonArray.length();
+
+        ArrayList<Uri> arrayList = new ArrayList<>();
+
+        JSONObject jsonSingleObject;
+
+        for (int i = 0; i < length; i++) {
+
+            jsonSingleObject = jsonArray.getJSONObject(i);
+
+            String v = jsonSingleObject.getString(VIDEO_TRAILERS_KEY);
+
+            Uri uri = NetWorkUtils.buildUrlForYoutube(v);
+
+            arrayList.add(i, uri);
+
+        }
+
         return new MovieTrailers(arrayList);
+
     }
 }
